b219e57941a230f4f35fd57f62ef4d6a294ea201
alexjli <alexjli@berkeley.edu>
2025-01-22 00:16:39 -0800

    initial functional warp ipa kernel

--- opt_kernels/kernels/warp_ipa.py
+++ opt_kernels/kernels/warp_ipa.py
@@ -3,12 +3,13 @@ import warp as wp
 import numpy as np
 
 # TODO: i'm not sure we need to be able to change this
-BLOCK_SIZE = 4
+BLOCK_SIZE = 8
 PARALLEL_BLOCK_SIZE = 1
 SERIAL_BLOCK_SIZE = 32
 # BLOCK_SIZE = 2
 W_L = float(np.sqrt(1/3))
 INF = float(1e3)
+NUM_HEADS = 2
 
 tensor_type = wp.array(dtype=float, ndim=3)
 vec3_tensor_type = wp.array(dtype=wp.vec3, ndim=4)
@@ -27,83 +28,89 @@ def generate_fwd_kernel(channel_dim, bias_dim, dtype):
     bias_tensor_type = wp.array(dtype=bias_vector, ndim=3)
     n_qk_pts = 8
     n_v_pts = 12
+    num_heads = NUM_HEADS
+
+    @wp.func
+    def square(x: float):
+        return wp.pow(x, 2.)
 
     @wp.kernel
     def ipa_sdpa_fwd(
         q: channel_tensor_type,             # [B, H, Q, C]
-        # q_pts: vec3_tensor_type,    # [B, H, Q, QK_pts, 3]
+        q_pts: channel_tensor_type,    # [B, H * QK_pts, Q, 3]
         k: channel_tensor_type,             # [B, H, KV, C]
-        # k_pts: vec3_tensor_type,    # [B, H, KV, QK_pts, 3]
+        k_pts: channel_tensor_type,    # [B, H * QK_pts, KV, 3]
         v: channel_tensor_type,             # [B, H, KV, C]
-        # v_pts: dim5_tensor_type,    # [B, H, V_pts, KV, 3]
+        v_pts: channel_tensor_type,    # [B, H * V_pts, KV, 3]
         z_attn_bias: wp.array(dtype=float, ndim=4),   # [B, H, Q, K]
         z_out_bias: channel_tensor_type,    # [B, Q, K, C_bias]
         out: channel_tensor_type,           # [B, H, Q, C]
-        # out_pts: dim5_tensor_type,  # [B, H, V_pts, Q, 3]
+        out_pts: channel_tensor_type,  # [B, H * V_pts, Q 3]
         out_bias: channel_tensor_type,      # [B, H, Q, C_bias]
         L: wp.array3d(dtype=float),             # [B, H, Q]
         M: wp.array3d(dtype=float),             # [B, H, Q]
-        pts_bias_scale: wp.array(dtype=float)     # [H]
+        pts_bias_scale: wp.array(dtype=float),     # [H]
+        num_k_blocks: int
     ):
         # define a bunch of necessary constants
         # n_qk_pts = q_pts.shape[3]
         # n_v_pts = v_pts.shape[3]
-        len_q = q.shape[3]
-        len_k = k.shape[3]
-        num_k_blocks = int(wp.ceil(float(len_k) / float(BLOCK_SIZE)))
-        c_qk = q.shape[2]
+        # len_q = q.shape[2]
+        # len_k = k.shape[2]
+        # num_k_blocks = int(wp.ceil(float(len_k) / float(BLOCK_SIZE)))
+        c_qk = q.shape[3]
         w_C = wp.sqrt(2. / (9. * float(n_qk_pts)))
         sdpa_scale = 1. / wp.sqrt(float(c_qk))
 
         batch, head, block_i = wp.tid()
 
-        len_i = wp.min(len_q - block_i * BLOCK_SIZE, BLOCK_SIZE)
+        # len_i = wp.min(len_q - block_i * BLOCK_SIZE, BLOCK_SIZE)
 
         block_q = q[batch, head]
         block_k = k[batch, head]
         block_v = v[batch, head]
-        # block_q_pts = q_pts[batch, head]
-        # block_k_pts = k_pts[batch, head]
-        # block_v_pts = v_pts[batch, head]
-        block_out_bias = z_out_bias[batch]
+        block_q_pts = q_pts[batch]
+        block_k_pts = k_pts[batch]
+        block_v_pts = v_pts[batch]
         block_attn_bias = z_attn_bias[batch, head]
         block_L = L[batch, head]
         block_M = M[batch, head]
         block_out = out[batch, head]
+        block_out_bias = out_bias[batch, head]
+        block_out_pts = out_pts[batch]
 
-        out_bias_tile = wp.tile_zeros(BLOCK_SIZE, bias_dim, dtype=float)
 
         for block_j in range(num_k_blocks):
             m = wp.tile_transpose(wp.tile_load(block_M, block_i, BLOCK_SIZE))
             l = wp.tile_transpose(wp.tile_load(block_L, block_i, BLOCK_SIZE))
             out_tile = wp.tile_load(block_out, block_i, 0, BLOCK_SIZE, channel_dim)
 
-            # pt_dist = wp.tile_zeros(BLOCK_SIZE, BLOCK_SIZE, dtype=float)
-            # pt_dist_buffer = wp.tile_zeros(BLOCK_SIZE, BLOCK_SIZE, dtype=float)
+            pt_dist = wp.tile_zeros(BLOCK_SIZE, BLOCK_SIZE, dtype=float)
 
-            # q_pts_tile = wp.tile_load(block_q_pts, block_i, 0, BLOCK_SIZE, n_qk_pts)
-            # k_pts_tile = wp.tile_load(block_k_pts, block_j, 0, BLOCK_SIZE, n_qk_pts)
+            for p in range(n_qk_pts):
+                head_p = head * n_qk_pts + p
+                q_pts_tile = wp.tile_load(block_q_pts[head_p], block_i, 0, BLOCK_SIZE, 3)
+                k_pts_tile = wp.tile_load(block_k_pts[head_p], block_j, 0, BLOCK_SIZE, 3)
+                k_pts_tile_T = wp.tile_transpose(k_pts_tile)
+                q_pts_tile_T = wp.tile_transpose(q_pts_tile)
 
-            # for p in range(n_qk_pts):
-            #     pt_dist_buffer *= 0.
-            #     pt_q = wp.tile_broadcast(
-            #         wp.tile_transpose(q_pts_tile)[p],
-            #         m=BLOCK_SIZE,
-            #         n=BLOCK_SIZE
-            #     )
-            #     pt_k = wp.tile_broadcast(
-            #         wp.tile_transpose(wp.tile_transpose(k_pts_tile)[p]),
-            #         m=BLOCK_SIZE,
-            #         n=BLOCK_SIZE
-            #     )
-            #     pt_disp = wp.tile_map(wp.sub, pt_q, pt_k)
+                q_pts_tile_T_sq = wp.tile_map(square, q_pts_tile_T)
+                q_pts_norm = wp.tile_transpose(
+                    q_pts_tile_T_sq[0] + q_pts_tile_T_sq[1] + q_pts_tile_T_sq[2]
+                )
+                k_pts_tile_T_sq = wp.tile_map(square, k_pts_tile_T)
+                k_pts_norm = k_pts_tile_T_sq[0] + k_pts_tile_T_sq[1] + k_pts_tile_T_sq[2]
 
-            #     pt_dist_update = wp.tile_map(wp.length_sq, pt_disp)
-            #     wp.tile_assign(pt_dist_buffer, 0, 0, pt_dist_update)
-            #     pt_dist += pt_dist_buffer
+                pt_dist_update = (
+                    -2.0 * wp.tile_matmul(q_pts_tile, k_pts_tile_T)
+                    + wp.tile_broadcast(q_pts_norm, BLOCK_SIZE, BLOCK_SIZE)
+                    + wp.tile_broadcast(k_pts_norm, BLOCK_SIZE, BLOCK_SIZE)
+                )
+
+                pt_dist += pt_dist_update
 
             # # add negative sign here
-            # pt_dist *= pts_bias_scale[head] * w_C / (-2.)
+            pt_dist *= pts_bias_scale[head] * w_C / (-2.)
 
             q_tile = wp.tile_load(block_q, block_i, 0, BLOCK_SIZE, channel_dim)
             k_tile = wp.tile_load(block_k, block_j, 0, BLOCK_SIZE, channel_dim)
@@ -114,7 +121,7 @@ def generate_fwd_kernel(channel_dim, bias_dim, dtype):
             s_ij = W_L * (
                 sdpa_scale * dpa
                 + bias
-                # + pt_dist
+                + pt_dist
             )
             # print(s_ij)
 
@@ -135,7 +142,7 @@ def generate_fwd_kernel(channel_dim, bias_dim, dtype):
             for j in range(BLOCK_SIZE):
                 tile_l += wp.tile_transpose(p_ij_T[j])
 
-            print(tile_l)
+            # print(tile_l)
 
             m_new = wp.tile_map(wp.max, m, tile_m)
             l_new = (
@@ -163,8 +170,8 @@ def generate_fwd_kernel(channel_dim, bias_dim, dtype):
             )
             new_scale = wp.tile_map(wp.mul, new_scale, p_ij)
             channel_div_scale = wp.tile_broadcast(l_new, m=BLOCK_SIZE, n=channel_dim)
-            # pt_div_scale = wp.tile_broadcast(l_new, m=BLOCK_SIZE, n=3)
-            # bias_div_scale = wp.tile_broadcast(l_new, m=BLOCK_SIZE, n=bias_dim)
+            pt_div_scale = wp.tile_broadcast(l_new, m=BLOCK_SIZE, n=3)
+            bias_div_scale = wp.tile_broadcast(l_new, m=BLOCK_SIZE, n=bias_dim)
 
             old_scale_diag = wp.tile_zeros(BLOCK_SIZE, BLOCK_SIZE, dtype=float)
             for i in range(BLOCK_SIZE):
@@ -177,39 +184,39 @@ def generate_fwd_kernel(channel_dim, bias_dim, dtype):
                     wp.tile_load(block_v, block_j, 0, BLOCK_SIZE, channel_dim)
                 )
             )
-            print(wp.tile_map(wp.div, new_scale, wp.tile_broadcast(l_new, m=BLOCK_SIZE, n=BLOCK_SIZE)))
+            # print(wp.tile_map(wp.div, new_scale, wp.tile_broadcast(l_new, m=BLOCK_SIZE, n=BLOCK_SIZE)))
             out_tile = wp.tile_map(wp.div, out_tile, channel_div_scale)
 
-            # for pt_v in range(n_v_pts):
-            #     out_pts_tile = wp.tile_load(out_pts[batch, head, pt_v], block_i, 0, BLOCK_SIZE, 3)
-            #     out_pts_tile = (
-            #         wp.tile_matmul(old_scale_diag, out_pts_tile) +
-            #         wp.tile_matmul(
-            #             new_scale,
-            #             wp.tile_load(block_v_pts[pt_v], block_j, 0, BLOCK_SIZE, 3)
-            #         )
-            #     )
-            #     out_pts_tile = wp.tile_map(wp.div, out_pts_tile, pt_div_scale)
-            #     wp.tile_store(out_pts[batch, head, pt_v], block_i, 0, out_pts_tile)
+            for p in range(n_v_pts):
+                head_p = head * n_v_pts + p
+                out_pts_tile = wp.tile_load(block_out_pts[head_p], block_i, 0, BLOCK_SIZE, 3)
+                out_pts_tile = (
+                    wp.tile_matmul(old_scale_diag, out_pts_tile) +
+                    wp.tile_matmul(
+                        new_scale,
+                        wp.tile_load(block_v_pts[head_p], block_j, 0, BLOCK_SIZE, 3)
+                    )
+                )
+                out_pts_tile = wp.tile_map(wp.div, out_pts_tile, pt_div_scale)
+                wp.tile_store(block_out_pts[head_p], block_i, 0, out_pts_tile)
 
-            # for i in range(len_i):
-            #     block_out_bias = z_out_bias[batch, block_i * BLOCK_SIZE + i]
-            #     update = wp.tile_load(block_out_bias, block_j, 0, BLOCK_SIZE, bias_dim)
-            #     update = wp.tile_matmul(
-            #         new_scale[i],
-            #         update
-            #     )
-            #     out_bias_tile_row = out_bias_tile[i]
-            #     new_row = (
-            #         wp.tile_matmul(old_scale[i], out_bias_tile_row)
-            #         + update
-            #     )
-            #     new_row = wp.tile_map(wp.div, new_row, bias_div_scale[i])
-            #     wp.tile_assign(out_bias_tile_row, 0, 0, new_row)
+            out_bias_tile = wp.tile_load(block_out_bias, block_i, 0, BLOCK_SIZE, bias_dim)
+            update_tile = wp.tile_zeros(BLOCK_SIZE, bias_dim, dtype=float)
+            for i in range(BLOCK_SIZE):
+                block_z_out_bias = z_out_bias[batch, block_i * BLOCK_SIZE + i]
+                update = wp.tile_load(block_z_out_bias, block_j, 0, BLOCK_SIZE, bias_dim)
+                update = wp.tile_matmul(
+                    new_scale[i],
+                    update
+                )
+                wp.tile_assign(update_tile[i], 0, 0, update)
+            out_bias_tile = wp.tile_matmul(old_scale_diag, out_bias_tile) + update_tile
+            out_bias_tile = wp.tile_map(wp.div, out_bias_tile, bias_div_scale)
 
-            wp.tile_store(block_L, block_i, l_new)
-            wp.tile_store(block_M, block_i, m_new)
+            wp.tile_store(block_L, block_i, wp.tile_transpose(l_new))
+            wp.tile_store(block_M, block_i, wp.tile_transpose(m_new))
             wp.tile_store(block_out, block_i, 0, out_tile)
+            wp.tile_store(block_out_bias, block_i, 0, out_bias_tile)
 
         m = wp.tile_transpose(wp.tile_load(block_M, block_i, BLOCK_SIZE))
         l = wp.tile_transpose(wp.tile_load(block_L, block_i, BLOCK_SIZE))
@@ -413,6 +420,7 @@ if __name__ == '__main__':
     import warp as wp
     import numpy as np
     import math
+    import time
 
     if torch.cuda.is_available():
         device = 'cuda:0'
@@ -422,21 +430,21 @@ if __name__ == '__main__':
     wp.init()
 
     B = 1
-    H = 1
-    L = 4
-    D = 4
+    H = NUM_HEADS
+    L = 64
+    D = 32
     D_bias = 16
-    qk_pts = 8
-    v_pts = 12
+    n_qk_pts = 8
+    n_v_pts = 12
 
     ipa_sdpa_fwd = generate_fwd_kernel(channel_dim=D, bias_dim=D_bias, dtype=float)
 
     q = torch.randn((B, H, L, D), dtype=torch.float32, device=device)
     k = torch.randn((B, H, L, D), dtype=torch.float32, device=device)
     v = torch.randn((B, H, L, D), dtype=torch.float32, device=device)
-    q_pts = torch.randn((B, H, L, qk_pts, 3), dtype=torch.float32, device=device)
-    k_pts = torch.randn((B, H, L, qk_pts, 3), dtype=torch.float32, device=device)
-    v_pts = torch.randn((B, H, v_pts, L, 3), dtype=torch.float32, device=device)
+    q_pts = torch.randn((B, H * n_qk_pts, L, 3), dtype=torch.float32, device=device)
+    k_pts = torch.randn((B, H * n_qk_pts, L, 3), dtype=torch.float32, device=device)
+    v_pts = torch.randn((B, H * n_v_pts, L, 3), dtype=torch.float32, device=device)
     z_attn_bias = torch.randn((B, H, L, L), dtype=torch.float32, device=device)
     z_out_bias = torch.randn((B, L, L, D_bias), dtype=torch.float32, device=device)
     pts_bias_scale = torch.ones(H, dtype=torch.float32, device=device)
@@ -450,34 +458,82 @@ if __name__ == '__main__':
 
     torch_inputs = [
             q, #.transpose(-1, -2),
-            # q_pts,
+            q_pts,
             k, #.transpose(-1, -2),
-            # k_pts,
+            k_pts,
             v, #.transpose(-1, -2),
-            # v_pts,
+            v_pts,
             z_attn_bias,
             z_out_bias,
             out,
-            # out_pts,
+            out_pts,
             out_bias,
             lse_store,
             m_store,
-            pts_bias_scale
+            pts_bias_scale,
+            math.ceil(L / BLOCK_SIZE)
     ]
-    wp_inputs = []
-    for idx, t in enumerate(torch_inputs):
-        print(idx)
-        wp_inputs.append(wp.from_torch(t, return_ctype=True))
+    # wp_inputs = []
+    # for idx, t in enumerate(torch_inputs):
+    #     print(idx)
+    #     wp_inputs.append(wp.from_torch(t, return_ctype=True))
 
     print("warp kernel")
     wp.launch_tiled(
         ipa_sdpa_fwd,
         dim=(B, H, math.ceil(L / BLOCK_SIZE)),
-        inputs=wp_inputs,
-        block_dim=64
+        inputs=torch_inputs,
+        block_dim=256
     )
+
+    L = 256
+    D = 128
+
+    q = torch.randn((B, H, L, D), dtype=torch.float32, device=device)
+    k = torch.randn((B, H, L, D), dtype=torch.float32, device=device)
+    v = torch.randn((B, H, L, D), dtype=torch.float32, device=device)
+    q_pts = torch.randn((B, H * n_qk_pts, L, 3), dtype=torch.float32, device=device)
+    k_pts = torch.randn((B, H * n_qk_pts, L, 3), dtype=torch.float32, device=device)
+    v_pts = torch.randn((B, H * n_v_pts, L, 3), dtype=torch.float32, device=device)
+    z_attn_bias = torch.randn((B, H, L, L), dtype=torch.float32, device=device)
+    z_out_bias = torch.randn((B, L, L, D_bias), dtype=torch.float32, device=device)
+    pts_bias_scale = torch.ones(H, dtype=torch.float32, device=device)
+
+    out = torch.zeros_like(q)
+    out_pts = torch.zeros_like(v_pts)
+    out_bias = torch.zeros((B, H, L, D_bias), dtype=torch.float32, device=device)
+
+    lse_store = torch.zeros((B, H, L), dtype=torch.float32, device=device)
+    m_store = torch.full((B, H, L), fill_value=-INF, dtype=torch.float32, device=device)
+
+    torch_inputs = [
+            q, #.transpose(-1, -2),
+            q_pts,
+            k, #.transpose(-1, -2),
+            k_pts,
+            v, #.transpose(-1, -2),
+            v_pts,
+            z_attn_bias,
+            z_out_bias,
+            out,
+            out_pts,
+            out_bias,
+            lse_store,
+            m_store,
+            pts_bias_scale,
+            math.ceil(L / BLOCK_SIZE)
+    ]
+    start = time.time()
+    wp.launch_tiled(
+        ipa_sdpa_fwd,
+        dim=(B, H, math.ceil(L / BLOCK_SIZE)),
+        inputs=torch_inputs,
+        block_dim=256
+    )
+    end = time.time()
+    print("time", end-start)
     # print(out)
-    print(lse_store)
+    # print(lse_store)
 
     # q_grad = torch.zeros_like(q)
     # k_grad = torch.zeros_like(k)
@@ -531,6 +587,13 @@ if __name__ == '__main__':
     #     t.requires_grad = True
 
     print("native pytorch")
+
+    #(B, H * qk_pts, L, 3) -> (B, H, L, qk_pts, 3)
+    q_pts = q_pts.unflatten(1, (H, n_qk_pts)).transpose(-2, -3)
+    k_pts = k_pts.unflatten(1, (H, n_qk_pts)).transpose(-2, -3)
+    v_pts = v_pts.unflatten(1, (H, n_v_pts)).transpose(-2, -3)
+
+    start = time.time()
     w_C = np.sqrt(2/(9*q_pts.shape[-2]))
     attn = torch.einsum("...ik,...jk->...ij", q, k) * 1/np.sqrt(q.shape[-1])
     attn = attn + z_attn_bias
@@ -540,30 +603,39 @@ if __name__ == '__main__':
         dim=-1
     )
     pts_bias = pts_dist.sum(dim=-1)
-    pts_bias *= 0
+    # pts_bias *= 0
 
     attn = attn - pts_bias * pts_bias_scale[None, :, None, None] * w_C / 2
     attn = attn * W_L
+    s_ij = attn
+    # print(s_ij)
     p_ij = torch.exp(attn - attn.max(dim=-1)[0][..., None])
     tile_l = p_ij.sum(dim=-1)
     attn = torch.softmax(attn, dim=-1)
     torch_out = torch.einsum("...ij,...jk->...ik", attn, v)
-    # torch_out_pts = torch.einsum("...ij,...jpk->...ipk", attn, v_pts)
+    torch_out_pts = torch.einsum("...ij,...jpk->...ipk", attn, v_pts)
     torch_out_bias = torch.sum(
         attn[..., None] * z_out_bias[..., None, :, :, :],
         dim=-2
     )
+    end = time.time()
+    print("time", end-start)
     # print(out_bias.shape, torch_out_bias.shape)
     # torch.autograd.backward(
     #     tensors=[torch_out, torch_out_pts, torch_out_bias],
     #     grad_tensors=[out_grad, out_pts_grad, out_bias_grad],
     # )
     # forward pass check
-    print(out[0, 0])
-    print(torch_out[0, 0])
+    # print(out[0])
+    # print(torch_out[0])
     assert torch.isclose(out, torch_out, atol=1e-6).all()
-    # assert torch.isclose(out_pts, torch_out_pts, atol=1e-6).all()
-    # assert torch.isclose(out_bias, torch_out_bias, atol=1e-6).all()
+    out_pts = out_pts.unflatten(1, (H, n_v_pts)).transpose(-2, -3)
+    print(out_pts[0])
+    print(torch_out_pts[0])
+    assert torch.isclose(out_pts, torch_out_pts, atol=1e-6).all()
+    # print(out_bias[0])
+    # print(torch_out_bias[0])
+    assert torch.isclose(out_bias, torch_out_bias, atol=1e-6).all()
     # # backward pass check
     # assert torch.isclose(v_grad, v.grad, atol=1e-6).all()
     # assert torch.isclose(v_pts_grad, v_pts.grad, atol=1e-6).all()
